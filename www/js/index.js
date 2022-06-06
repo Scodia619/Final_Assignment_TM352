c4fb7e0d37abc33005dbe1eb8b16a4f5060af75e
Scodia619 <billyjoe2701@gmail.com>
2022-06-06 19:43:48 +0100

    v1.2

--- www/js/index.js
+++ www/js/index.js
@@ -1,24 +1,37 @@
 //Decides which picture it will display first for the widget
 var picnum = 1;
-
 //Initializes the order Array
 var orderDescription = [];
 //will show the order for list purposes with child node
 var orderedItems = [];
 //shows how many of a certain item has been order
 var orderItemsTotal = [0,0,0,0,0,0,0,0,0,0]
-//total price
-var orderTotal = 0;
+//Subtotal price
+var orderSubotal = 0.0;
+//Total Price
+var orderTotal = 0.0;
 //total vat
-var orderVAT = 0;
+var orderVAT = 0.0;
 //for the list of items added to the database
 var newLi = document.createElement("li");
-
 //will be uesd to count which element of the array is needed
 var counter = 0;
-
 //sets the table im going to use
 var list = document.getElementById("ordered")
+//sets the longitude and latitude variables
+var lat = 0;
+var long = 0;
+//These are the password, username and clientId
+var id = "";
+var pass = "";
+var clientId = "";
+//Client Info
+var cInfo = [];
+//Todays date and then the correct format.
+var date = new Date();
+date = date.toISOString().split('T')[0]
+//array for orders made for today
+var ordersToday = []
 
 //Sets the first widget for the webpage
 let request = new XMLHttpRequest()
@@ -39,8 +52,22 @@ request.onload = () => {
   }
 }
 
+//Getting the orders for the exact day for use in Begin Order FR2.2
+request.open("GET", "http://137.108.92.9/openstack/api/orders?OUCU=zx403061&password=ilSR098R");
+
+request.send();
+
+request.onload = () => {
+
+  if(request.status === 200){
+    var data = JSON.parse(request.response);
+  }else{
+    console.log("Page Not Found")
+  }
+}
+
 //Trying to get the plaform to work MAP API
-var platform = new H.serice.Platform({
+var platform = new H.service.Platform({
     'apikey': "eYvI5v6nt48inOZIPdVmM9oEPgbM8KQ8LK8O8_OTVVo"
 })
 
@@ -65,41 +92,53 @@ var map = new H.Map(
       center: {lat: 35.68026, lng: 139.76744}
     });
 
- function endorder(){
-	console.log("Hello")
-}
-
 //Begin order function
 function begin(){
 
-  //sets the variable for a new request
-  let request = new XMLHttpRequest()
-  //refreshes order array
-  myArr = [];
+  //gets the long and lat position of where the order is being taken
+  navigator.geolocation.getCurrentPosition(function(position){
+
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+
+    console.log(lat, long)
+  })
+
+  id = document.getElementById("id").value;
+  pass = document.getElementById("pass").value;
+  clientId = document.getElementById("clientId").value;
+
+  //FR1.1 Validing the OUCU
+  const regex = new RegExp(/^[a-z]{1}[a-z0-9]{6}[0-9]{1}$/);
   
-  //zx40306, ilSR098R
+  if(regex.test(id)){
+    //sets the variable for a new request
+    let request = new XMLHttpRequest()
+    //refreshes order array
+    myArr = [];
+    
+    //sets the get request
+    request.open("GET", "http://137.108.92.9/openstack/api/clients/" + clientId +"?OUCU="+ id +"&password="+pass);
 
-  let id = document.getElementById("id").value;
-  let pass = document.getElementById("pass").value;
-  let clientId = document.getElementById("clientId").value;
+    request.send();
 
+    request.onload = () => {
 
-  request.open("GET", "http://137.108.92.9/openstack/api/clients/" + clientId +"?OUCU="+ id +"&password="+pass+"");
+      if(request.status === 200){
+        data = JSON.parse(request.response)
+        document.getElementById("order").innerHTML = "Dear, " + data.data[0].name + ", Your order at: " + data.data[0].address;
+      }else{
+        console.log("Page Not Found")
+      }
 
-  request.send();
-
-  request.onload = () => {
-
-    if(request.status === 200){
-      console.log(JSON.parse(request.response))
-    }else{
-      console.log("Page Not Found")
-    }
-
-  } 
+    } 
+  }else{
+    console.log("Doesnt match a correct OUCU")
+  }  
 }
 
 //next item widget button navigation
+//FR1.2 Navigating between widgets
 function next(){
 
   let request = new XMLHttpRequest()
@@ -131,6 +170,7 @@ function next(){
 }
 
 //previous item widget button navigation
+//FR1.2 Navigating the Widgets
 function prev(){
 
   let request = new XMLHttpRequest()
@@ -160,6 +200,8 @@ function prev(){
 
 }
 
+
+//FR1.3&FR1.4 Adding current widget to the order, Changing Sum and VAT of order
 function addOrder(){
 
   let request = new XMLHttpRequest()
@@ -174,32 +216,45 @@ function addOrder(){
       var data = JSON.parse(request.response);
       //adds order data to the order array
       orderDescription.push([data.data[0].description, data.data[0].pence_price]);
-      console.log(orderDescription);
       //adds to the total items
       orderItemsTotal[(data.data[0].id)-1] += 1;
-      console.log(orderItemsTotal)
       if(orderedItems.includes(data.data[0].id)){
         pass;
       }else{
         orderedItems.push(data.data[0].id);
       }
+
+      //Working out Prices for subtotal
+      var price = parseFloat(data.data[0].pence_price)/100
+      orderSubotal += price
+      orderSubotal = parseFloat(orderSubotal.toFixed(2))
+
+      //works out VAT
+      orderVAT = orderSubotal / 5
+      orderVAT = parseFloat(orderVAT.toFixed(2))
+
+      //Works out Order Total
+      orderTotal = orderSubotal + orderVAT
+      orderTotal = parseFloat(orderTotal.toFixed(2))
+
       //adds to the list
-      //TRYING TO FIND AN ELEMENTS INDEX WITHIN AN ARRAY
       var index = orderedItems.indexOf(data.data[0].id)
-      console.log("this is the index of the current widget in the orderedItems array: " + index)
-      console.log((data.data[0].id)-1)
-      if(orderItemsTotal[(data.data[0].id)-1] > 1){
-        //gets the element that will be edited
-        var element = document.getElementById("ordered").childNodes[0];
-        console.log(element)
-        //sets what the ned list item will be
-        var newNode = document.createTextNode(orderDescription[counter] + "(" + orderItemsTotal[(data.data[0].id)-1] + ")")
-        //then replaces the list item
-        element.replaceChild(newNode, element.childNodes[index])
+
+      //sets the new element
+      var newLi = document.createElement("li");
+      var indexMany = (data.data[0].id)-1
+      newLi.innerHTML = orderDescription[counter] + "(" + orderItemsTotal[indexMany] + ")";
+
+      if(orderItemsTotal[indexMany] > 1){
+        list.replaceChild(newLi, list.childNodes[index])
       }else{
-        newLi.innerHTML = orderDescription[counter] + "(" + orderItemsTotal[(data.data[0].id)-1] + ")";
         ordered.appendChild(newLi);
       }
+
+      //Changes order Totals
+      document.getElementById("subtot").innerHTML = "Subtotal: £" + orderSubotal + "GBP"
+      document.getElementById("vat").innerHTML = "VAT: £" + orderVAT + "GBP"
+      document.getElementById("tot").innerHTML = "Total: £" + orderTotal + "GBP"
       //adds to the counter ready for the next addition
       counter ++;
     }else{
@@ -207,4 +262,32 @@ function addOrder(){
     }
 
     }
+}
+
+//FR1.5 Posting an order to the API
+function endOrder(){
+  
+  var data = new FormData();
+  data.append("OUCU", id);
+  data.append("password", pass);
+  data.append("client_id", clientId);
+  data.append("latitude", String(lat));
+  data.append("longitude", String(long));
+  
+  var xhr = new XMLHttpRequest();
+  
+  xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+      console.log(this.responseText);
+    }
+  });
+  
+  xhr.open("POST", "http://137.108.92.9/openstack/api/orders");
+  
+  xhr.send(data);
+
+  //FR2.1 sets the map to current location
+  map.setCenter({lat: String(lat), lng:String(long)});
+  map.setZoom(14);
+
 }
